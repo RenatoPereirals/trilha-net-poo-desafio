c12b7dde59fb02f754abc27a7ceac85358d894c3
RenatoPereirals <renatopreirals@gmail.com>
2024-01-05 18:07:53 -0300

    Instanciando as Classes Iphone e Nokia

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new("123456", "Nokia 8", "IMEI123", 64);
+Iphone iphone = new("654321", "iPhone X", "IMEI456", 128);
+
+Smartphone.Ligar();
+Smartphone.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Smartphone.Ligar();
+Smartphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
