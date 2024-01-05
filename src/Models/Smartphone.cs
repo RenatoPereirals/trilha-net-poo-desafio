e8dc9aa4361ef0123fe2dac23a7792920522f844
RenatoPereirals <renatopreirals@gmail.com>
2024-01-05 18:06:17 -0300

    Criando propriedades e o construtor

---
+++ src/Models/Smartphone.cs
@@ -0,0 +1,32 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        public static void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public static void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
