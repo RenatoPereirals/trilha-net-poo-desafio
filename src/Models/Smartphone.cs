f6ebcc73cd4bd977b486c48fa857a97129e0ae04
RenatoPereirals <renatopreirals@gmail.com>
2024-01-05 18:40:15 -0300

    Remoção de comentários

--- src/Models/Smartphone.cs
+++ src/Models/Smartphone.cs
@@ -6,7 +6,6 @@ namespace DesafioPOO.Models
         public string Modelo { get; private set; }
         public string IMEI { get; private set; }
         public int Memoria { get; private set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
@@ -14,7 +13,6 @@ namespace DesafioPOO.Models
             Modelo = modelo;
             IMEI = imei;
             Memoria = memoria;
-            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
